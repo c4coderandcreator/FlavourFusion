a04ca11b87c91342ae1dc948cc90c74e177d82ed
liwwil <cutanyasanganan@gmail.com>
2023-08-09 23:31:56 +0700

    Created Sign In Sign Up

---
+++ javascript/signInUp.js
@@ -0,0 +1,30 @@
+function toggleSignUp() {
+    const h1 = document.getElementById("signIn-Up-hd1");
+    const h2 = document.getElementById("signIn-Up-hd2");
+    const h3 = document.getElementById("signIn-Up-hd3");
+    const h4 = document.getElementById("signIn-Up-hd4");
+    h1.classList.toggle("d-none");
+    h1.classList.toggle("fIn");
+    h2.classList.toggle("d-none");
+    h2.classList.toggle("fOut");
+    h3.classList.toggle("d-none");
+    h3.classList.toggle("fOut");
+    h4.classList.toggle("d-none");
+    h4.classList.toggle("fIn");
+  }
+  
+  function toggleSignIn() {
+    const h1 = document.getElementById("signIn-Up-hd1");
+    const h2 = document.getElementById("signIn-Up-hd2");
+    const h3 = document.getElementById("signIn-Up-hd3");
+    const h4 = document.getElementById("signIn-Up-hd4");
+    h1.classList.toggle("d-none");
+    h1.classList.toggle("fOut");
+    h2.classList.toggle("d-none");
+    h2.classList.toggle("fIn");
+    h3.classList.toggle("d-none");
+    h3.classList.toggle("fIn");
+    h4.classList.toggle("d-none");
+    h4.classList.toggle("fOut");
+  }
+  
