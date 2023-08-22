273b4172791a3b02ce010c8123474d42d5a36cee
Saurashya <Saurashya1@gmail.com>
2023-08-22 21:13:02 +0545

    Changes made to the responsiveness of orderNow and Track,SAM

--- track-sam.js
+++ track-sam.js
@@ -4,8 +4,34 @@ const closeTrack=document.getElementById('close-track-section');
 
 trackReveal.addEventListener('click',()=>{
     trackSection.classList.add('active-sam');
+    removeContent();
 });
 
 closeTrack.addEventListener('click',()=>{
     trackSection.classList.remove('active-sam');
+    revealContent();
 });
+
+//remove sam sections
+function removeSamSection(){
+    trackSection.classList.remove('active-sam');
+    content.classList.remove("active-sam");
+    revealContent();
+}
+document.getElementById("menu-btn").addEventListener("click", () => {
+    removeSamSection();
+    });
+document.getElementById("home-btn").addEventListener("click", () => {
+    removeSamSection();
+    });
+document.getElementById("allab").addEventListener("click", () => {
+    removeSamSection();
+    });
+document.getElementById("aboutLink").addEventListener("click", () => {
+    removeSamSection();
+    });
+document.getElementById("sign-in-btn").addEventListener("click", () => {
+    removeSamSection();
+    });
+
+   
